fb8e52c8af2f57334f8e3ccebe73ab69f316630c
masaru514 <415urasam@gmail.com>
2020-06-17 22:34:59 +0900

    [add]初めての型

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View,ScrollView,Image,Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+//propsのオブジェクト引数の型決める? = interface
+interface ButtonName {
+  name: string;
+  time: string;
+}
+
+//Buttonコンポーネント
+function TimeButton(props: ButtonName) {
+  //状態
+  const [time ,setTime] = useState(0);
+  const plusTime = function() {
+    setTime(time + 1)
+  }
+
+  //ループ処理させたい
+  function Timer() {
+      setInterval(plusTime, 1000)
+      console.log("a")
+    
+  }
+
+  //Buttonが押すと、数字が変わる
+  return(
+    <View>
+      <Text>{time}</Text>
+      <Button onPress={() => Timer} title={props.name} />
     </View>
   );
 }
+export default function App(props: any) {
+  return(
+      <View style={styles.container}>
+        <TimeButton name="ボタンです"/>
+        <ActiveButton />
+      </View>
+    );
+}  
+function ActiveButton(){
+  let obj = {name: 'tuyoi'}
+  return(
+    <Button onPress={() => Alert.alert(obj.name)} title='インターフェース確認' />
+  )
+}
+
 
 const styles = StyleSheet.create({
   container: {
